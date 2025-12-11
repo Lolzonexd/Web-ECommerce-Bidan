a0375308e57f6da8311a869e115a1c6129f75549
A. Hanif Nursyabana <ahanifnursyabana08@gmail.com>
2025-12-11 11:18:38 +0700

    Benerinn no WA di index.php + Dokumentasi di page/login.php & page/register.php

--- page/register.php
+++ page/register.php
@@ -3,18 +3,21 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar - PMB Nurhasanah</title>
-
-    <link rel="stylesheet" href="../asset/style.css">
-    
+    <!-- POPPINS FONT -->
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <!-- AWESOME FONT -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../asset/style.css">
 </head>
-<body>
 
+<body>
+    <!-- NAVBAR -->
     <header>
         <div class="logo">
             <i class="fas fa-heartbeat"></i> PMB Nurhasanah
@@ -25,7 +28,9 @@ session_start();
             </ul>
         </nav>
     </header>
+    <!-- END NAVBAR -->
 
+    <!-- CARD LOGIN -->
     <div class="auth-wrapper">
         <div class="auth-box">
             <div class="auth-header">
@@ -64,6 +69,11 @@ session_start();
             </div>
         </div>
     </div>
+    <!-- END CARD LOGIN -->
+
+    <!-- FOOTER -->
+    <?php include '../layout/footer.html'; ?>
 
 </body>
+
 </html>
