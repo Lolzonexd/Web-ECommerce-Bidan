162c839efb9b03b866529f5d6717cda9ce8ada70
Rasputois <comraden69@gmail.com>
2025-12-30 17:23:27 +0700

    key

--- service/midtrans_config.php
+++ service/midtrans_config.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/../vendor/autoload.php'; 
 
 // Set konfigurasi Midtrans
-\Midtrans\Config::$serverKey = 'Mid-server-R-EagaH4ndx7Q4pPZQnLpDlK'; //Server key midtrans
+\Midtrans\Config::$serverKey = ''; //Server key midtrans
 \Midtrans\Config::$isProduction = false; // False karena mode Sandbox
 \Midtrans\Config::$isSanitized = true;
 \Midtrans\Config::$is3ds = true;
