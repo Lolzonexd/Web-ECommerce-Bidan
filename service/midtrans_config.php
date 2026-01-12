0311f6dd8ce5d26879f30a8ee18d7bdb2807a27b
A. Hanif Nursyabana <ahanifnursyabana08@gmail.com>
2026-01-12 17:00:55 +0700

    Exclude /config/midtras_config.php

--- service/midtrans_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/../vendor/autoload.php'; 
-
-// Set konfigurasi Midtrans
-\Midtrans\Config::$serverKey = ''; //Server key midtrans
-\Midtrans\Config::$isProduction = false; // False karena mode Sandbox
-\Midtrans\Config::$isSanitized = true;
-\Midtrans\Config::$is3ds = true;
-?>
